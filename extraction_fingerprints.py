64e8775eda2d0e309900cd7034c2681c4233220c
Alessio Belli <alessio.belli@studenti.unitn.it>
2023-04-15 16:57:56 +0200

    Accuracy using euclidian distance

--- extraction_fingerprints.py
+++ extraction_fingerprints.py
@@ -3,12 +3,14 @@
 from sys import argv
 import os
 from time import time
+
 from numpy.fft import fft2, ifft2
 import numpy as np
 from glob import glob
 from noiseprint.noiseprint import genNoiseprint
 from noiseprint.utility.utilityRead import imread2f
 from noiseprint.utility.utilityRead import jpeg_qtableinv
+from sklearn.metrics import roc_curve, auc, accuracy_score
 
 directory = "./data/"
 fingerprints = "./fingerprints/"
@@ -66,7 +68,7 @@ def pce(cc: np.ndarray, neigh_radius: int = 2) -> dict:
 
     return out
 
-def create_fingerprint():
+def training():
     ff_dirlist = np.array(sorted(glob('data/*')))
     ff_device = np.array([os.path.split(i)[1].rsplit('_', 1)[0] for i in ff_dirlist])
     fingerprint_device = sorted(np.unique(ff_device))
@@ -89,7 +91,55 @@ def create_fingerprint():
         temp = np.average(a, axis=0)
         np.save("./fingerprints_resized/"+device, temp)
     
+def gt_function(l1: list or np.ndarray, l2: list or np.ndarray) -> np.ndarray:
+    """
+    Determine the Ground Truth matrix given the labels
+    :param l1: fingerprints labels
+    :param l2: residuals labels
+    :return: groundtruth matrix
+    """
+    l1 = np.array(l1)
+    l2 = np.array(l2)
+
+    assert (l1.ndim == 1)
+    assert (l2.ndim == 1)
+
+    gt_arr = np.zeros((len(l1), len(l2)), np.bool)
+
+    for l1idx, l1sample in enumerate(l1):
+        gt_arr[l1idx, l2 == l1sample] = True
     
+    return gt_arr
+
+def stats(cc: np.ndarray, gt: np.ndarray, ) -> dict:
+    """
+    Compute statistics
+    :param cc: cross-correlation or normalized cross-correlation matrix
+    :param gt: boolean multidimensional array representing groundtruth
+    :return: statistics dictionary
+    """
+    assert (cc.shape == gt.shape)
+    assert (gt.dtype == np.bool)
+
+    assert (cc.shape == gt.shape)
+    assert (gt.dtype == np.bool)
+
+    fpr, tpr, th = roc_curve(gt.flatten(), cc.flatten())
+    auc_score = auc(fpr, tpr)
+
+    # EER
+    eer_idx = np.argmin((fpr - (1 - tpr)) ** 2, axis=0)
+    eer = float(fpr[eer_idx])
+
+    outdict = {
+        'tpr': tpr,
+        'fpr': fpr,
+        'th': th,
+        'auc': auc_score,
+        'eer': eer,
+    }
+
+    return outdict
 
 def correlation():
     list_test_images = os.listdir("./test/")
@@ -114,7 +164,7 @@ def correlation():
                     min_score = dist
                     name_distance = device
                 
-            
+            print(dist)
             print(image+" ---> "+name+" using the correlation")
             print(image+" ---> "+name_distance+" using the euclidian distance")
 
@@ -125,8 +175,63 @@ def correlation():
         except:
             print("")
 
+def testing():
+    
+    ff_dirlist = np.array(sorted(glob('fingerprints_resized/*')))
+    ff_device = np.array([os.path.split(i)[1].rsplit('_', 1)[0] for i in ff_dirlist])
+    fingerprint_device = sorted(np.unique(ff_device))
+    k = []
+    for fingerprint in ff_dirlist:
+        k.append(np.load(fingerprint))
+
+    nat_dirlist = np.array(sorted(glob('./test/*.jpg')))
+    nat_device = np.array([os.path.split(i)[1].rsplit('_', 1)[0] for i in nat_dirlist])
+
+    w = []
+    for img_path in nat_dirlist:
+        img, mode = imread2f(img_path, channel=1)
+        try:
+            QF = jpeg_qtableinv(strimgfilenameeam)
+        except:
+            QF = 200
+        w+=[genNoiseprint(img,QF)]
+    
+    gt = gt_function(fingerprint_device, nat_device)
+    euclidian_rot = np.zeros((len(fingerprint_device), len(nat_device)))
+
+    for fingerprint_idx, fingerprint_k in enumerate(k):
+        for natural_idx, natural_w in enumerate(w):
+            dist = np.linalg.norm(fingerprint_k-natural_w)
+            euclidian_rot[fingerprint_idx, natural_idx] = dist
+
+    print(euclidian_rot)
+    accuracy = accuracy_score(gt.argmax(0), euclidian_rot.argmin(0))
+    print(accuracy)
+
+    '''
+    print('Computing PCE...')
+    pce_rot = np.zeros((len(fingerprint_device), len(nat_device)))
+
+    for fingerprint_idx, fingerprint_k in enumerate(k):
+        natural_indices = []
+        for natural_idx, natural_w in enumerate(w):
+            dist = np.linalg.norm(fingerprint_k-natural_w)
+            cc2d = crosscorr_2d(fingerprint_k, natural_w)
+            prnu_pce = pce(cc2d)['pce']
+            pce_rot[fingerprint_idx, natural_idx] = dist
+            natural_indices.append(natural_idx)
+
+    print(gt)
+    print(pce_rot)
+    print('Computing statistics on PCE...')
+    stats_pce = stats(pce_rot, gt)
+
+    print('AUC on PCE {:.2f}'.format(stats_pce['auc']))
+    '''
+    
 
 
 if __name__ == "__main__":
-    #create_fingerprint()
-    correlation()
+    #training()
+    #correlation()
+    testing()
