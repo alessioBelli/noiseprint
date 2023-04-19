b9d9c6ad6d0f7a68e4c5e251e9317526979386a0
Alessio Belli <58253493+alessioBelli@users.noreply.github.com>
2023-04-19 14:07:06 +0200

    Random_crop

--- main_extraction.py
+++ main_extraction.py
@@ -23,6 +23,7 @@ from noiseprint.utility.functions import cut_ctr, crosscorr_2d, pce, stats, gt, 
 import numpy as np
 import glob
 from PIL import Image
+from random import randrange
 import os
 from sklearn import metrics
 from sklearn.metrics import accuracy_score, confusion_matrix
@@ -30,6 +31,7 @@ import matplotlib
 matplotlib.use('TkAgg')
 import matplotlib.pyplot as plt
 from sklearn.metrics import ConfusionMatrixDisplay
+import tqdm
 '''
 imgfilename = argv[1]
 outfilename = argv[2]
@@ -62,6 +64,27 @@ fingerprint_device = sorted(np.unique(ff_device))
 nat_dirlist = np.array(sorted(glob.glob('data/test/*')))
 nat_device = np.array([os.path.split(i)[1].rsplit('_', 1)[0] for i in nat_dirlist])
 
+def random_crop(img, crop_size):
+    img = img.copy()
+    for axis in range(img.ndim):
+        axis_target_size = crop_size[axis]
+        axis_original_size = img.shape[axis]
+        if axis_target_size > axis_original_size:
+            raise ValueError(
+                'Can\'t have target size {} for axis {} with original size {}'.format(axis_target_size, axis,
+                                                                                      axis_original_size))
+        elif axis_target_size < axis_original_size:
+            axis_start_idx = randrange(0,axis_original_size - crop_size[axis])
+            axis_end_idx = axis_start_idx + crop_size[axis]
+            img = np.take(img, np.arange(axis_start_idx, axis_end_idx), axis)
+
+    '''
+    plt.imshow(img, interpolation='nearest')
+    plt.show()
+    '''
+
+    return img
+
 def compute_noiseprints(crop_size):
     print('Computing fingerprints...')
     # for each device, we extract the images belonging to that device and we compute the corresponding noiseprint, which is saved in the array k
@@ -71,7 +94,8 @@ def compute_noiseprints(crop_size):
         noises = []
         for img_path in ff_dirlist[ff_device == device]:
             img, mode = imread2f(img_path, channel=1)
-            img = cut_ctr(img, crop_size)
+            #img = cut_ctr(img, crop_size)
+            img = random_crop(img, crop_size)
             try:
                 QF = jpeg_qtableinv(strimgfilenameeam)
             except:
@@ -95,9 +119,10 @@ def compute_residuals(crop_size):
     print('Computing residuals...')
     # for each device, we extract the images belonging to that device and we compute the corresponding noiseprint, which is saved in the array w
     w=[]
-    for img_path in nat_dirlist:
+    for img_path in tqdm.tqdm(nat_dirlist):
         img, mode = imread2f(img_path, channel=1)
-        img = cut_ctr(img, crop_size)
+        #img = cut_ctr(img, crop_size)
+        img = random_crop(img, crop_size)
         try:
             QF = jpeg_qtableinv(strimgfilenameeam)
         except:
